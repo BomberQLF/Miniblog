952644b6fea2a930ca22650c50a9db5fef6d8b3f
BomberQLF <tmurphyparis@gmail.com>
2024-11-08 09:59:58 +0100

    Ajout d'une photo de profile

--- Controller/index.php
+++ Controller/index.php
@@ -141,10 +141,10 @@ switch ($action) {
         }
 
     case 'blogDetails':
-            $postId = $_GET['id'];
-            $post = showPostById($postId);
-            $comments = showComments($postId);
-            include('../Vue/blogDetails.php');
+        $postId = $_GET['id'];
+        $post = showPostById($postId);
+        $comments = showComments($postId);
+        include('../Vue/blogDetails.php');
         break;
 
 
@@ -194,6 +194,59 @@ switch ($action) {
             }
         }
         break;
+
+    case 'upload':
+        if (isLoggedIn()) {
+            $target_dir = "/Applications/MAMP/htdocs/Miniblog/uploads/";
+            $imageFileType = strtolower(pathinfo($_FILES["photo_profile"]["name"], PATHINFO_EXTENSION));
+            $id_personne = $_SESSION['user_id'];
+
+            // Créer un nom de fichier unique
+            $uniqueFileName = $id_personne . "_" . time() . "." . $imageFileType;
+            $target_file = $target_dir . $uniqueFileName;
+
+            $uploadOk = 1;
+
+            // Vérifiez les erreurs d'upload
+            if ($_FILES["photo_profile"]["error"] != 0) {
+                echo "Erreur lors de l'upload du fichier.";
+                $uploadOk = 0;
+            }
+
+            // Vérifiez la taille du fichier
+            if ($_FILES["photo_profile"]["size"] > 500000) {
+                echo "Désolé, votre fichier est trop volumineux.";
+                $uploadOk = 0;
+            }
+
+            // Vérifiez les formats de fichier autorisés
+            if (!in_array($imageFileType, ["jpg", "jpeg", "png", "gif"])) {
+                echo "Désolé, seuls les fichiers JPG, JPEG, PNG & GIF sont autorisés.";
+                $uploadOk = 0;
+            }
+
+            // Si tout est bon, déplacer le fichier dans uploads
+            if ($uploadOk == 1) {
+                // Récupérer le nom actuel de la photo de profil dans la base de données
+                $oldFileName = getCurrentProfilePicture($id_personne);
+
+                if (move_uploaded_file($_FILES["photo_profile"]["tmp_name"], $target_file)) {
+                    // Supprimez l'ancienne image, si elle existe
+                    if ($oldFileName && file_exists($target_dir . $oldFileName)) {
+                        unlink($target_dir . $oldFileName);
+                    }
+
+                    // Mettre à jour la base de données avec le nouveau nom de fichier
+                    uploadProfilePicture($uniqueFileName, $id_personne);
+
+                } else {
+                    echo "Désolé, une erreur est survenue lors du téléchargement de votre fichier.";
+                }
+            }
+        }
+        include('../Vue/gestionProfile.php');
+        break;
+
 
     default:
         include('../Vue/miniblog.php');
