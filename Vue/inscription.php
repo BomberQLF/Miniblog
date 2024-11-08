b401cce38f5865f80aeb590ebe00c6621aa6bfa0
BomberQLF <tmurphyparis@gmail.com>
2024-11-08 08:42:32 +0100

    Mise en forme de l'inscription

--- Vue/inscription.php
+++ Vue/inscription.php
@@ -3,26 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/style.css">
     <title>Inscription</title>
 </head>
-<body>
-    <form method="POST" action="/Miniblog/Controller/index.php?action=inscription">
-        <label for="login">Login</label><br>
-        <input type="text" name="login" id="login" placeholder="Nom d'utilisateur" required><br>
+<body class="signup-body">
+    <form method="POST" action="/Miniblog/Controller/index.php?action=inscription" class="signup-form">
+        <h1 class="signup-title">Inscription</h1>
 
-        <label for="prenom">Prenom</label><br>
-        <input type="text" name="prenom" id="prenom" placeholder="Prenom" required><br>
+        <label for="login" class="signup-label">Login</label>
+        <input type="text" name="login" id="login" placeholder="Nom d'utilisateur" class="signup-input" required>
 
-        <label for="nom">Nom</label><br>
-        <input type="text" name="nom" id="nom" placeholder="Nom" required><br>
+        <label for="prenom" class="signup-label">Prénom</label>
+        <input type="text" name="prenom" id="prenom" placeholder="Prénom" class="signup-input" required>
 
-        <label for="mot_de_passe">Mot de passe</label><br>
-        <input type="password" name="mot_de_passe" id="mot_de_passe" required><br>
+        <label for="nom" class="signup-label">Nom</label>
+        <input type="text" name="nom" id="nom" placeholder="Nom" class="signup-input" required>
 
-        <label for="confirm_mot_de_passe">Confirmer le mot de passe</label><br>
-        <input type="password" name="confirm_mot_de_passe" id="confirm_mot_de_passe" required><br>
+        <label for="mot_de_passe" class="signup-label">Mot de passe</label>
+        <input type="password" name="mot_de_passe" id="mot_de_passe" class="signup-input" required>
 
-        <input type="submit" value="S'inscrire">
+        <label for="confirm_mot_de_passe" class="signup-label">Confirmer le mot de passe</label>
+        <input type="password" name="confirm_mot_de_passe" id="confirm_mot_de_passe" class="signup-input" required>
+
+        <input type="submit" value="S'inscrire" class="signup-button">
     </form>
 </body>
 </html>
